0809e0734776926cf961d52b6bdbf6be3fbf8619
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-08-03 09:24:22 +0200

    initial spring-boot files

---
+++ src/main/java/org/demo/springbootbds/SpringbootbdsApplication.java
@@ -0,0 +1,13 @@
+package org.demo.springbootbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/demo/springbootbds/SpringbootbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.springbootbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
