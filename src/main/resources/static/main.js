c86cfcbb521d83d45e1eec3e7407b423056e9b12
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-08-04 07:35:35 +0200

    integrated baloise design system with demo page

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,6 @@
+import { balIconConsultant } from './webjars/baloise-design-system/@baloise/ds-icons/index.esm.js';
+import { balBrandIconTheftCarGreen } from './webjars/baloise-design-system/@baloise/ds-brand-icons/index.esm.js';
+
+document.getElementById('balIconConsultant').svg = balIconConsultant;
+document.getElementById('balBrandIconTheftCarGreen').svg = balBrandIconTheftCarGreen;
+
