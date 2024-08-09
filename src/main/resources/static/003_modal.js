f1ab4c6d61fd286f72855dfded5fdc2d685000a9
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-08-09 07:04:38 +0200

    using modal.open() instead of modal.present() works for html-based usage of BDS

--- src/main/resources/static/003_modal.js
+++ src/main/resources/static/003_modal.js
@@ -1,22 +1,7 @@
-import { balIconConsultant } from './webjars/baloise-design-system/@baloise/ds-icons/index.esm.js';
-import { balBrandIconTheftCarGreen } from './webjars/baloise-design-system/@baloise/ds-brand-icons/index.esm.js';
-
 const openButton = document.getElementById('openBtn');
-// const modal = document.getElementById('modal');
+const modal = document.getElementById('modal');
 
-(async () => {
-	await customElements.whenDefined('bal-modal')
-	// const todoListElement = document.querySelector('bal-select')
-	// await todoListElement.select('tennis')
-	const modal = document.getElementById('modal');
-	openButton.addEventListener('click', async (event) => {
-		console.log('listener', event);
-		await modal.present();
-	});
-
-})()
-
-// openButton.addEventListener('click', (event) => {
-// 	console.log('listener', event);
-// 	modal.present();
-// });
+openButton.addEventListener('click', (event) => {
+	console.log('listener', event);
+	modal.open();
+});
