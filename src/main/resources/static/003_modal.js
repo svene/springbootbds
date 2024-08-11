c9948baac4c7c64d358a687b2801c9afee163da0
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-08-11 08:55:08 +0200

    modal: "return" data when pressing ok button

--- src/main/resources/static/003_modal.js
+++ src/main/resources/static/003_modal.js
@@ -1,7 +1,27 @@
 const openButton = document.getElementById('openBtn');
 const modal = document.getElementById('modal');
+const okBtn = document.getElementById('okBtn');
+const cancelBtn = document.getElementById('cancelBtn');
 
 openButton.addEventListener('click', (event) => {
-	console.log('listener', event);
 	modal.open();
+});
+okBtn.addEventListener('click', (event) => {
+	// Unfortnuately data is not used by BDS-modal in html/js mode:
+	// modal.dismiss({name: 'Bart'}, 'role');
+	// Therefore a custom event will be used here as a workaround:
+	const x = new CustomEvent('app_modal-ok', { detail: {name: 'Bart'} });
+	modal.dispatchEvent(x);
+	modal.dismiss();
+});
+modal.addEventListener('balModalWillDismiss', (event) => {
+	console.log('balModalWillDismiss', event);
+});
+modal.addEventListener('app_modal-ok', (event) => {
+	console.log('app_modal-ok', event.detail);
+});
+modal.onWillDismiss().then(d => console.log('onWillDismiss: ', d));
+
+cancelBtn.addEventListener('click', (event) => {
+	modal.dismiss();
 });
